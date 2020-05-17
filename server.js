31f440c4465dad9e570260339ba88a3ed54a9a5d
Ayush Puri <38379529+puriayush29@users.noreply.github.com>
2020-05-17 21:20:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,16 +25,16 @@ const friends = require('./routes/friendsRoutes');
 const message = require('./routes/messageRoutes');
 const image = require('./routes/imageRoutes');
 
-// app.use((req, res, next) => {
-// 	res.header('Access-Control-Allow-Origin', '*');
-// 	res.header('Access-Control-Allow-Credentials', 'true');
-// 	res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'DELETE', 'PUT');
-// 	res.header(
-// 		'Access-Control-Allow-Headers',
-// 		'Origin, X-Requested-With, Content-Type, Accept'
-// 	);
-// 	next();
-// });
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'DELETE', 'PUT');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
